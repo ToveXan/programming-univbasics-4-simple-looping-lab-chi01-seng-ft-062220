96b6894f3998448480bd64815f94a3af180d1e5d
Zoe Xanos <zxanos@gmail.com>
2020-05-08 09:43:19 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -33,10 +33,12 @@ output_array(array)
 return_string_array(array)
   count = 0 
   while count < array.length do
+    array[count].to_s
     count += 1 
   end
 end
 
-
+new_array = [5, 4, 3, 2, 1]
+return_string_array(new_array)
 
 
